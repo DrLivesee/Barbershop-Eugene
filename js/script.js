d710eb4392a7f95ad09cb8338d8d6de94e160290
jason parser <cucya100@gmail.com>
2020-04-10 23:00:57 +0300

    добавил первый тестовый скрипт

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let picture = document.querySelector('.pic1 img')
+let popupWrapper = document.querySelector('.popup-wrapper')
+
+let isAlreadyClicked = false
+
+picture.addEventListener('click', () => {
+    if (isAlreadyClicked === false) {
+        popupWrapper.style.display = 'flex'
+    } 
+})
+
+popupWrapper.addEventListener('click', () => {
+    popupWrapper.style.display = 'none'
+    isAlreadyClicked = true
+})
